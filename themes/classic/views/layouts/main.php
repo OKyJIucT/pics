efe3afa598e9d695456e98298b2c913eeb1d1f70
Kohone <okyjiuct@gmail.com>
2015-01-18 20:16:11 +0200

    yandex verification

--- themes/classic/views/layouts/main.php
+++ themes/classic/views/layouts/main.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name='yandex-verification' content='7cf750433f4878e9' />
         <?php
         Yii::app()->clientScript->registerCssFile(
             Yii::app()->assetManager->publish(
